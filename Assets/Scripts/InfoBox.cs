5091693a7577e7cb86bbbd5c293286f3bed5f7e1
Sean Taylor <silverwolf4350@gmail.com>
2021-06-26 21:46:42 -0500

    Added comments and cleaned up code.

--- Assets/Scripts/InfoBox.cs
+++ Assets/Scripts/InfoBox.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+///<summary>Script that enables the easy toggling of an info box.</summary>
 public class InfoBox : MonoBehaviour
 {
+    ///<summary>Shows and hides the passed info box.</summary>
     public void ToggleActive(GameObject infoBox) {
         infoBox.SetActive(!infoBox.activeSelf);
     }
